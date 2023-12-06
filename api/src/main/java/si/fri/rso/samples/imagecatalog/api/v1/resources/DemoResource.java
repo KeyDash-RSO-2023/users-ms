45a4b57d18bb52032f62ec6829e425f9903ac9ed
tilenmiklavic <tm7321@student.uni-lj.si>
2023-12-06 15:47:43 +0100

    Update test demo

--- api/src/main/java/si/fri/rso/samples/imagecatalog/api/v1/resources/DemoResource.java
+++ api/src/main/java/si/fri/rso/samples/imagecatalog/api/v1/resources/DemoResource.java
@@ -1,13 +1,18 @@
 package si.fri.rso.samples.imagecatalog.api.v1.resources;
 
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.enums.SchemaType;
+import org.eclipse.microprofile.openapi.annotations.headers.Header;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
+import si.fri.rso.samples.imagecatalog.lib.User;
 import si.fri.rso.samples.imagecatalog.services.config.RestProperties;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.logging.Logger;
@@ -22,6 +27,21 @@ public class DemoResource {
 
     @Inject
     private RestProperties restProperties;
+
+    @Operation(description = "Get all demo metadata.", summary = "Get all demo")
+    @APIResponses({
+            @APIResponse(responseCode = "200",
+                    description = "List of demo metadata",
+                    content = @Content(schema = @Schema(implementation = User.class, type = SchemaType.ARRAY)),
+                    headers = {@Header(name = "X-Total-Count", description = "Number of objects in list")}
+            )})
+    @GET
+    public Response getDemoResponse() {
+
+        String responseContent = "demo";
+
+        return Response.status(Response.Status.OK).entity(responseContent).build();
+    }
 
     @POST
     @Path("break")
